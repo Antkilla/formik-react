2cbfca1e2c229c1400c5cb6106ec261a96e55251
Antkilla <132300802+Antkilla@users.noreply.github.com>
2024-01-24 17:55:18 -0800

    fix blank error was id=root solve error was create root module not found

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -1,31 +1,37 @@
 import React from 'react';
-import { Formik, Field, Form } from 'formik'
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import validationSchema from './ValidationSchema';
 
 const ContactForm = () => {
-    return (
-        <Formik
-            initialValues={{
-                name: '',
-                email: '',
-            }}
-            onSubmit={async (values) => {
-                await new Promise((r) => setTimeout(r, 500));
-                alert(JSON.stringify(values, null, 2));
-            }}
-        >
-            <Form>
+  return (
+    <Formik
+      initialValues={{
+        name: '',
+        email: '',
+      }}
+      onSubmit={async (values) => {
+        await new Promise((r) => setTimeout(r, 500));
+        alert(JSON.stringify(values, null, 2));
+      }}
+      ValidationSchema={validationSchema} 
+    >
+        <Form>
+            <div>
                 <label htmlFor="name">Name</label>
                 <Field id="name" name="name" placeholder="Jacob" />
+                <ErrorMessage name="name" component="div" />
+            </div>
 
+            <div>
                 <label htmlFor="email">Email</label>
                 <Field id="email" name="email" placeholder="Jacob@hotmail.com" type="email" />
-    
-                <button type="submit">Submit</button>
-            </Form>
-        </Formik>
-    );
+                <ErrorMessage name="email" component="div" />
+            </div>
+
+            <button type="submit">Submit</button>
+        </Form>
+    </Formik>
+  );
 };
 
 export default ContactForm;
-
-  
